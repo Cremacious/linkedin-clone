a7cecc1362d7f36bdbd6d6792b318d3bca4786dc
cremacious <chrismackall3@gmail.com>
2025-10-22 12:31:33 -0400

    Added post route and controller

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import userRoutes from './routes/user.route.js';
-
+import postRoutes from './routes/post.route.js';
 import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
@@ -24,6 +24,7 @@ app.use(express.json({ limit: '5mb' }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/posts', postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
