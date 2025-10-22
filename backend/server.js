7e3a3e4e5038662a8f4438d0ea607869c35f1aa8
cremacious <chrismackall3@gmail.com>
2025-10-22 13:31:30 -0400

    Added connections routes and controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import { connectDB } from './lib/db.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
 import authRoutes from './routes/auth.route.js';
+import notificationRoutes from './routes/notification.route.js';
+import connectionRoutes from './routes/connection.route.js';
+
 dotenv.config();
 
 const app = express();
@@ -25,6 +28,8 @@ app.use(express.json({ limit: '5mb' }));
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/notifications', notificationRoutes);
+app.use('/api/v1/connections', connectionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
