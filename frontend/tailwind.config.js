855b719a972def903ca73069cabbd215da3b861c
Cremacious <chrismackall3@gmail.com>
2025-10-22 18:35:11 -0400

    Updated sign up page and theming

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,4 +7,21 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        linkedin: {
+          primary: '#0A66C2',
+          secondary: '#FFFFFF',
+          accent: '#7FC15E',
+          neutral: '#000000',
+          'base-100': '#F3F2EF',
+          info: '#5E5E5E',
+          success: '#057642',
+          warning: '#F5C75D',
+          error: '#CC1016',
+        },
+      },
+    ],
+  },
 };
