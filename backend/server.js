7e103a9347cfd45f081eec005929b8fed5fcdccc
cremacious <chrismackall3@gmail.com>
2025-10-23 10:02:01 -0400

    Setup app for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { connectDB } from './lib/db.js';
+import path from 'path';
+
+import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
-import authRoutes from './routes/auth.route.js';
 import notificationRoutes from './routes/notification.route.js';
 import connectionRoutes from './routes/connection.route.js';
+
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV !== 'production') {
   app.use(
     cors({
@@ -20,10 +26,9 @@ if (process.env.NODE_ENV !== 'production') {
     })
   );
 }
-const PORT = process.env.PORT || 5000;
 
+app.use(express.json({ limit: '5mb' })); 
 app.use(cookieParser());
-app.use(express.json({ limit: '5mb' }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
@@ -31,7 +36,15 @@ app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
 app.use('/api/v1/connections', connectionRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   connectDB();
 });
